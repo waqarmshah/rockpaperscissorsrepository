0a535a9d9b411ebc56e6ad410598fecc2a439fe4
waqarmshah <waqarmshah@gmail.com>
2024-03-04 18:29:06 +0000

    Update manual_rps.py

--- manual_rps.py
+++ manual_rps.py
@@ -1,13 +1,18 @@
-import random #for random choices
+import random
+import cv2
+import numpy as np
+from keras.models import load_model
+import time
 
-#defining the function play
+model = load_model('keras_model.h5')
+
 def play():
     computer_score = 0
     user_score = 0
-#while loop to keep the game running
+
     while True:
         computer_choice = get_computer_choice()
-        user_choice = get_user_choice()
+        user_choice = countdown()
 
         print("Computer chose:", computer_choice)
         print("User chose:", user_choice)
@@ -23,7 +28,7 @@ def play():
             print("It is a tie!")
 
         print("Score: Computer", computer_score, "- User", user_score)
-#asking the user if they want to play again
+
         play_again = input("Do you want to play again? (y/n): ")
         if play_again.lower() != 'y':
             break
@@ -32,12 +37,49 @@ def get_computer_choice():
     choices = ['rock', 'paper', 'scissors']
     return random.choice(choices)
 
-def get_user_choice():
-    while True:
-        user_choice = input("Enter your choice (rock/paper/scissors): ")
-        if user_choice in ['rock', 'paper', 'scissors']:
-            return user_choice
-        print("Invalid choice. Enter again.")
+def get_prediction():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Failed to open the camera.")
+        return 'nothing'
+
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to capture a frame from the camera.")
+        cap.release()
+        return 'nothing'
+
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (224, 224))
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0
+
+    prediction = model.predict(img)[0]
+
+    cap.release()
+
+    classes = ['rock', 'paper', 'scissors', 'nothing']
+    predicted_class = classes[np.argmax(prediction)]
+    return predicted_class
+
+def countdown():
+    start_time = time.time()
+    countdown_duration = 3
+
+    while time.time() - start_time < countdown_duration:
+        current_time = time.time()
+        remaining_time = countdown_duration - (current_time - start_time)
+        print(f"Countdown: {remaining_time:.1f} seconds")
+
+        user_choice = get_prediction()
+        if user_choice == 'nothing':
+            print("Failed to capture a valid frame. Please try again.")
+            return None
+
+        time.sleep(0.1)
+
+    print(f"You chose {user_choice}")
+    return user_choice
 
 def get_winner(computer_choice, user_choice):
     winning_combinations = {
